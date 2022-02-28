0e057e53249079b042f2dceeffb1cf9c38701158
yafit <yafitmizrahi3@gmail.com>
2022-02-28 16:16:04 +0200

    adding file

---
+++ Program/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace A22_Ex02_Yafit_318861960_Lonnie_316223411
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            GameManagerUI game = new GameManagerUI();
+
+            game.PlayBullsAndCleots();
+        }
+
+    }
+}
---
+++ Program/SequenceAndFidback.cs
@@ -0,0 +1,40 @@
+﻿namespace A22_Ex02_Yafit_318861960_Lonnie_316223411
+{
+    public struct SequenceAndFeedback
+    {
+        private  string m_Sequence;
+        private string m_Feedback;
+
+        public SequenceAndFeedback(string i_Sequence, string i_Feedback)
+        {
+            m_Sequence = i_Sequence;
+            m_Feedback = i_Feedback;
+        }
+
+        public string Sequence
+        {
+            get
+            {
+                return m_Sequence;
+            }
+            set
+            {
+
+                m_Sequence = value;
+            }
+        }
+
+        public string Feedback
+        {
+            get
+            {
+                return m_Feedback;
+            }
+            set
+            {
+                m_Feedback = value;
+            }
+        }
+
+    }
+}
